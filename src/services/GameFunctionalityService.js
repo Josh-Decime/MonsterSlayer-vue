89aee45c6dbe1b23c67ce97f1a640a56c3a21b19
Josh Decime <josh_decime@yahoo.com>
2024-05-30 15:20:10 -0600

    set up GameFunctionalityService so I can clean up my code & better organize it

---
+++ src/services/GameFunctionalityService.js
@@ -0,0 +1,13 @@
+import { AppState } from "../AppState.js"
+import { Character } from "../models/Character.js"
+import { Monster } from "../models/Monster.js"
+import { characterService } from "./CharactersService.js"
+import { monsterService } from "./MonstersService.js"
+import Pop from "../utils/Pop.js"
+
+class GameFunctionalityService {
+
+
+}
+
+export const gameFunctionalityService = new GameFunctionalityService()
