eb12247cd47fbe9c748a17ae4404c83a0c3c7bc8
Josh Decime <josh_decime@yahoo.com>
2024-06-24 23:15:13 -0600

    Added player powers service

---
+++ src/services/PlayerPowersService.js
@@ -0,0 +1,14 @@
+import { AppState } from "../AppState.js";
+import { Character } from "../models/Character.js";
+import { Monster } from "../models/Monster.js";
+import { characterService } from "./CharactersService.js";
+import { monsterService } from "./MonstersService.js";
+import Pop from "../utils/Pop.js";
+
+class PlayerPowersService {
+    // NOTE Special moves the player can activate independent of their characters
+
+
+}
+
+export const playerPowersService = new PlayerPowersService()
