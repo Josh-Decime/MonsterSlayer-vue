27a16f920b57a468eb6d6e1c77c24d623555ccdf
Josh Decime <josh_decime@yahoo.com>
2024-07-29 15:46:05 -0600

    added store background

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,63 +1,68 @@
 <template>
-  <div class="about">
-    <h1>Welcome to the store</h1>
-    <h3>Your Coins: {{ yourCoins }}</h3>
-  </div>
-
-  <section class="container-fluid">
-    <section class="row">
-
-      <!-- SECTION display all heroes -->
-      <div v-for="hero in heroesForSale" class="col-4">
-        <img v-if="hero.img" :src="hero.img" :alt="hero.name" class="store-characters-img">
-        <h3>{{ hero.name }}: Lvl {{ hero.level }}</h3>
-        <p class="my-0">Health: {{ hero.health }}/{{ hero.maxHealth }}</p>
-        <p class="m-0">Damage: {{ hero.damage }}</p>
-
-        <!-- SECTION explain heroes special moves -->
-        <div>
-          <span>Special Move: </span>
-          <span v-if="hero.striker">Striker, costs 💪{{ hero.strikeCost }} to deal {{ hero.strikeAmount }} damage</span>
-          <span v-if="hero.healer">Healer, costs 💪{{ hero.healCost }} to heal the team by +{{ hero.healAmount }}</span>
-          <span v-if="hero.healerOverTime">Heal over time, costs 💪{{ hero.healOverTimeCost }} to heal the team
-            +{{ hero.healOverTimeAmount }}/round for {{ hero.healOverTimeDuration }} rounds</span>
-          <span v-if="hero.shield">Shield, costs 💪{{ hero.shieldCost }} for the team to be immune to damage next
-            round</span>
-          <span v-if="hero.overcharge">Overcharge, use this round to increase 💪 by {{ hero.overchargeAmount }}</span>
-          <span v-if="hero.kamikaze">Kamikaze, costs 💪{{ hero.kamikazePowerCost }} & this hero loses
-            -{{ hero.kamikazeHealthCost }} health to deal {{ hero.kamikazeDamage }} damage</span>
-        </div>
-
-        <!-- SECTION Buy, equip, upgrade, heal heroes -->
-        <button v-if="hero.unlocked && !hero.dead" class="btn btn-primary"
-          :class="{ 'disabled': yourCoins < hero.upgradeCost }" @click="upgradeCharacter(hero)">
-          {{ hero.upgradeCost }} Upgrade</button>
-
-        <button v-if="!hero.unlocked" :class="{ 'btn btn-primary': true, 'disabled': yourCoins < hero.purchasePrice }"
-          @click="buyCharacter(hero)"> {{ hero.purchasePrice }} Buy</button>
-
-        <button v-if="hero.unlocked && hero.health < hero.maxHealth && !hero.dead" class="btn btn-success"
-          @click="potionHealCharacter(hero)">
-          {{ hero.potionCost }} Heal</button>
-
-        <button v-if="hero.dead" class="btn btn-primary" :class="{ 'disabled': yourCoins < hero.reviveCost }"
-          @click="reviveCharacter(hero)">Revive for {{ hero.reviveCost }} coins</button>
-
-        <button v-if="hero.unlocked && hero.equip == false" class="btn btn-success"
-          @click="equipCharacter(hero)">Equip</button>
-
-        <button v-if="hero.unlocked && hero.equip" class="btn btn-secondary"
-          @click="unEquipCharacter(hero)">Un-equip</button>
-      </div>
-    </section>
-  </section>
-
-  <!-- SECTION return to battle button -->
-  <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-    <div class="d-flex flex-column align-items-center">
-      <button class="fs-3 btn btn-success mt-3">Get back to cleaning</button>
+  <section class="background">
+    <div class="about">
+      <h1>Welcome to the store</h1>
+      <h3>Your Coins: {{ yourCoins }}</h3>
     </div>
-  </router-link>
+
+    <section class="container-fluid">
+      <section class="row">
+
+        <!-- SECTION display all heroes -->
+        <div v-for="hero in heroesForSale" class="col-4">
+          <img v-if="hero.img" :src="hero.img" :alt="hero.name" class="store-characters-img">
+          <h3>{{ hero.name }}: Lvl {{ hero.level }}</h3>
+          <p class="my-0">Health: {{ hero.health }}/{{ hero.maxHealth }}</p>
+          <p class="m-0">Damage: {{ hero.damage }}</p>
+
+          <!-- SECTION explain heroes special moves -->
+          <div>
+            <span>Special Move: </span>
+            <span v-if="hero.striker">Striker, costs 💪{{ hero.strikeCost }} to deal {{ hero.strikeAmount }}
+              damage</span>
+            <span v-if="hero.healer">Healer, costs 💪{{ hero.healCost }} to heal the team by +{{ hero.healAmount
+              }}</span>
+            <span v-if="hero.healerOverTime">Heal over time, costs 💪{{ hero.healOverTimeCost }} to heal the team
+              +{{ hero.healOverTimeAmount }}/round for {{ hero.healOverTimeDuration }} rounds</span>
+            <span v-if="hero.shield">Shield, costs 💪{{ hero.shieldCost }} for the team to be immune to damage next
+              round</span>
+            <span v-if="hero.overcharge">Overcharge, use this round to increase 💪 by {{ hero.overchargeAmount }}</span>
+            <span v-if="hero.kamikaze">Kamikaze, costs 💪{{ hero.kamikazePowerCost }} & this hero loses
+              -{{ hero.kamikazeHealthCost }} health to deal {{ hero.kamikazeDamage }} damage</span>
+          </div>
+
+          <!-- SECTION Buy, equip, upgrade, heal heroes -->
+          <button v-if="hero.unlocked && !hero.dead" class="btn btn-primary"
+            :class="{ 'disabled': yourCoins < hero.upgradeCost }" @click="upgradeCharacter(hero)">
+            {{ hero.upgradeCost }} Upgrade</button>
+
+          <button v-if="!hero.unlocked" :class="{ 'btn btn-primary': true, 'disabled': yourCoins < hero.purchasePrice }"
+            @click="buyCharacter(hero)"> {{ hero.purchasePrice }} Buy</button>
+
+          <button v-if="hero.unlocked && hero.health < hero.maxHealth && !hero.dead" class="btn btn-success"
+            @click="potionHealCharacter(hero)">
+            {{ hero.potionCost }} Heal</button>
+
+          <button v-if="hero.dead" class="btn btn-primary" :class="{ 'disabled': yourCoins < hero.reviveCost }"
+            @click="reviveCharacter(hero)">Revive for {{ hero.reviveCost }} coins</button>
+
+          <button v-if="hero.unlocked && hero.equip == false" class="btn btn-success"
+            @click="equipCharacter(hero)">Equip</button>
+
+          <button v-if="hero.unlocked && hero.equip" class="btn btn-secondary"
+            @click="unEquipCharacter(hero)">Un-equip</button>
+        </div>
+      </section>
+    </section>
+
+    <!-- SECTION return to battle button -->
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <div class="d-flex flex-column align-items-center">
+        <button class="fs-3 btn btn-success mt-3">Get back to cleaning</button>
+      </div>
+    </router-link>
+
+  </section>
 </template>
 
 <script>
@@ -131,5 +136,30 @@ export default {
 <style scoped lang="scss">
 .store-characters-img {
   height: 20vh;
+}
+
+.background {
+  height: 120vh;
+  background-image: url('src/assets/img/CleaningStore.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  color: rgb(255, 255, 255);
+}
+
+.background::before {
+  content: '';
+  position: absolute;
+  top: 10;
+  left: 0;
+  width: 100%;
+  height: 120vh;
+  background: rgba(72, 72, 72, 0.5);
+  z-index: 1;
+}
+
+.background>* {
+  position: relative;
+  z-index: 2;
 }
 </style>
