72dfe2b8ae463559951c96532499257c0352bfe3
Josh Decime <josh_decime@yahoo.com>
2024-10-10 19:02:37 -0600

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,32 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   build: {
+//     outDir: 'docs',
+//     sourcemap: false
+//   },
+//   server: {
+//     port: 8080
+//   }
+// })
+
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
 export default defineConfig({
-  plugins: [vue()],
-  build: {
-    outDir: 'docs',
-    sourcemap: false
-  },
-  server: {
-    port: 8080
+  base: '/MonsterSlayer-vue/', // Replace with your GitHub repository name
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   }
 })
